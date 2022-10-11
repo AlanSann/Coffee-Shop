edf4b63d0e27c13b0b4a2125c43ecac23384254b
Alan Kouakou <alan.kouakou@edu.esiee-it.fr>
2022-10-11 13:52:48 +0200

    The begining of the Coffee Shop

---
+++ main.py
@@ -0,0 +1,13 @@
+print("\nHello, welcome to BetterHumanity Coffee Shop!\n ")
+
+name = input("What is your name ?\n")
+
+print("Hello " + name+ ", thank you so much for coming in today.\n")
+
+menu =  "Black Coffee, Espresso, Latte, Capuccino"
+
+print(name + ", what would you like to drink today ? Here is what we are serving :\n" + menu )
+
+order = input()
+
+print("Sounds good " + name + ", we will have that " + order + " ready for you in a moment.")
